eefcd31d96334a4e47637d025594dae0ce227079
TimoteoSantos <santastimoteo21@gmail.com>
2026-01-22 05:25:43 -0300

    trabalhando com arrays parte 1

--- ProjectLivro/untitled/src/br/com/casadocodigo/livraria/produtos/CarrinhoDeCompras.java
+++ ProjectLivro/untitled/src/br/com/casadocodigo/livraria/produtos/CarrinhoDeCompras.java
@@ -1,13 +1,17 @@
 package br.com.casadocodigo.livraria.produtos;
 
 public class CarrinhoDeCompras {
+    //criando um atributo do tipo Produto privado a essa classe
+    private Produto[] produtos = new Produto[10];
     //um atributo que guarda o total
     private double total;
+    private int contador = 0;
 
         //envio para adicionar qualquer produto independente do seu tipo Ebook Livro fisico ou Revista
         public void adiciona(Produto produto){
-
         System.out.println("Adicionando: " + produto);
+        this.produtos[contador] = produto; //adicionar na posicao contador um produto recebido no argumento
+        contador++;// andar uma posição do contador atual
         total += produto.getValor();// todos os objetos passados tem esse metodo porque foram obrigados pela interface Produto
             //que é o tipo do objeto passado
     }
